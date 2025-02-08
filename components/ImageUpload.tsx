6a5c2da37cf3e74dc0e11c745efdb9898a96cea7
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2025-02-08 23:31:05 +0100

    update

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,5 +1,30 @@
-import React from 'react'
+'use client';
 
+import config from "@/lib/config";
+import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
+
+
+const authenticator = async () => {
+    try {
+        const response  =  await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+        if(!response.ok){
+            const errorText = await response.text();
+
+            throw new Error(
+                `Request failed with status ${response.status}: ${errorText}`
+            );
+        }
+        
+        const data = await response.json();
+
+        const { signature, expire, token } = data;
+
+        return { token, signature, expire };
+    } catch (error: any) {
+        throw new Error(`Authentication request failed: ${error.message}`);
+    }
+};
 const ImageUpload = () => {
   return (
     <div>ImageUpload</div>
--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,10 @@
 const config = {
     env: {
+        apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit: {
-            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_KEY,
-            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
-            privateKey: process.env.IMAGEKIT_PRIVATE_KIT,
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_KEY!,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KIT!,
         }
     }
 }
