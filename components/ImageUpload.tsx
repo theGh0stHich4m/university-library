160e977f6f8aea23459da629ded694c374221139
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2025-02-08 23:43:10 +0100

    update

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,34 +1,44 @@
-'use client';
+"use client";
 
 import config from "@/lib/config";
+import ImageKit from "imagekit";
 import { IKImage, ImageKitProvider, IKUpload } from "imagekitio-next";
 
+const {
+  env: {
+    imagekit: { publicKey, urlEndpoint },
+  },
+} = config;
 
 const authenticator = async () => {
-    try {
-        const response  =  await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+  try {
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
 
-        if(!response.ok){
-            const errorText = await response.text();
+    if (!response.ok) {
+      const errorText = await response.text();
 
-            throw new Error(
-                `Request failed with status ${response.status}: ${errorText}`
-            );
-        }
-        
-        const data = await response.json();
-
-        const { signature, expire, token } = data;
-
-        return { token, signature, expire };
-    } catch (error: any) {
-        throw new Error(`Authentication request failed: ${error.message}`);
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
     }
+
+    const data = await response.json();
+
+    const { signature, expire, token } = data;
+
+    return { token, signature, expire };
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`);
+  }
 };
 const ImageUpload = () => {
   return (
-    <div>ImageUpload</div>
-  )
-}
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    ></ImageKitProvider>
+  );
+};
 
-export default ImageUpload
+export default ImageUpload;
--- lib/config.ts
+++ lib/config.ts
@@ -2,9 +2,9 @@ const config = {
     env: {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
         imagekit: {
-            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_KEY!,
-            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+            publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             privateKey: process.env.IMAGEKIT_PRIVATE_KIT!,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
         }
     }
 }
