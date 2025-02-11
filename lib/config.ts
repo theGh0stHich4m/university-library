00345fcb19b5850c4290c87781dde56c454d15f7
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2025-02-11 19:43:34 +0100

    update

--- lib/config.ts
+++ lib/config.ts
@@ -5,7 +5,8 @@ const config = {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
             privateKey: process.env.IMAGEKIT_PRIVATE_KIT!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
-        }
+        },
+        databaseUrl: process.env.DATABASE_URL
     }
 }
 
