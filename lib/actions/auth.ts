17c117a7c92aa4a1f3f7d3244121402565cbc9cd
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2025-03-02 18:18:37 +0000

    updates

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import ratelimit from "../ratelimit";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -65,6 +67,14 @@ export const signUp = async (params: AuthCredentials) => {
       universityCard,
       password: hashedPassword,
     });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
+      body: {
+        email,
+        fullName,
+      }
+    })
 
     await signInWithCredentials({ email, password });
     return { success: true };
