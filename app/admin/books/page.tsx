87884c0bd1e95bf24a9e5995b2cebba9d135a584
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2025-04-07 16:25:03 +0100

    Admin

---
+++ app/admin/books/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='w-full rounded-2xl bg-white p-7'>
+        <div className="flex flex-wrap items-center justify-between gap-2">
+            <h2 className="text-xl font-semibold">All Books</h2>
+            <Button className="bg-primary-admin" asChild>
+                <Link href="/admin/books/new" className='text-white'>
+                    + Create a new Book
+                </Link>
+            </Button>
+        </div>  
+    </section>
+  )
+}
+
+export default page
