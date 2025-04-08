1145e90b77c2094279b3e89ddf852df25a532e8a
XploitN1nj4 <hicham.ezzamzami@gmail.com>
2025-04-08 15:54:04 +0100

    add book to the db

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,25 +1,35 @@
-import { auth } from '@/auth';
-import { redirect } from 'next/navigation';
-import React, { ReactNode } from 'react'
-import '@/styles/admin.css';
-import Sidebar from '@/components/admin/Sidebar';
-import Header from '@/components/admin/Header';
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
+import "@/styles/admin.css";
+import Sidebar from "@/components/admin/Sidebar";
+import Header from "@/components/admin/Header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
-
   const session = await auth();
 
   if (!session?.user?.id) redirect("/sign-in");
-  return (
-    <main className='flex min-h-screen w-full flex-row'>
-        <Sidebar session={session} />
 
-        <div className="admin-container">
-            <Header session={session}/>
-            {children}
-        </div>
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user.id))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  if (!isAdmin) redirect("/");
+  return (
+    <main className="flex min-h-screen w-full flex-row">
+      <Sidebar session={session} />
+
+      <div className="admin-container">
+        <Header session={session} />
+        {children}
+      </div>
     </main>
-  )
-}
+  );
+};
 
 export default Layout;
